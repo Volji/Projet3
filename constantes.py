d330ced819edd772f55bd563d768974e81886efc
Volji <chipset@laposte.net>
2017-07-26 09:51:30 +0200

    modification du fichier constantes

--- constantes.py
+++ constantes.py
@@ -2,31 +2,43 @@ from tkinter import *
 
 class Constantes:
 	def __init__(self):
-		# Images of objects
+		# Images des objets
 		self.obj1 = PhotoImage(file = "data/obj1.gif")
 		self.obj2 = PhotoImage(file = "data/obj2.gif")
 		self.obj3 = PhotoImage(file = "data/obj3.gif")
-		# Image of the floor
+		# Image du sol
 		self.floor = PhotoImage(file="data/sol.gif") 
-		# Image of walls
+		# Image du mur
 		self.wall = PhotoImage(file="data/mur.gif")
-		# Image of the keeper
+		# Image du gardien
 		self.gard = PhotoImage(file="data/gardien.gif")
-		# Image of McGyver (player)
+		# Images du player
 		self.mcgyver1 = PhotoImage(file = "data/mcgyver.gif")
 		self.mcgyver2 = PhotoImage(file = "data/death.gif")
-		# Image of the beggining
+		# Image du début du jeu
 		self.photo1 = PhotoImage(file ='data/init.gif')
 		# variables
 		self.play = False
 		self.end_game = True
-		# image size
+		#self.board = [
+		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+		#]
+		# Dimension image
 		self.SIZE = 25
-		# Number of lines
+		# Nombre de ligne
 		self.LIGNS = 15
-		# Number of columns
+		# Nombre de colone
 		self.COLUMNS = 15
-		# table creation
+		# Création du tableau
 		self.board = [[0]*self.COLUMNS for i in range(self.LIGNS)]
 		
 		
